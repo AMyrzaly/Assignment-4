6b7eb9fdffd359c729b4c7d5dd28bdb06955047f
Imba-lol <best.abuka@gmail.com>
2017-07-23 11:58:40 -0400

    Added the Private Fields for the abstract class Planet

---
+++ Assignment 4/Planet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Abubakir Myrzaly
+ * Date: July 23, 2017
+ * Description: This is the abstract class Planet which other subclasses will
+ * derive from.
+ * Version: 0.1 - Added the Private Fields
+ */
+
+namespace Assignment_4
+{
+    public abstract class Planet
+    {
+        // PRIVATE FIELDS (INSTANCE VARIABLES)
+
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
+
+        // PUBLIC PROPERTIES
+
+        // CONSTRUCTORS
+
+        // PUBLIC ABSTRACT METHODS
+    }
+}
