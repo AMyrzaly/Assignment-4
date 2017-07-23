00c9ba6002b4bb7cb4b08f5e8d10b8b45a8d86aa
Imba-lol <best.abuka@gmail.com>
2017-07-23 13:00:04 -0400

    Created IHasRings interface and Created IHasMoons interface

---
+++ Assignment 4/IHasMoons.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Abubakir Myrzaly
+ * Date: July 23, 2017
+ * Description: This is an Interface that defines a Moons Property that
+ * must be implemented in any class that subscribes to it.
+ * Version: 0.1 - Created IHasMoons interface
+ */
+
+namespace Assignment_4
+{
+    public interface IHasMoons
+    {
+        int Moons { get; set; }
+    }
+}
---
+++ Assignment 4/IHasRings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Abubakir Myrzaly
+ * Date: July 23, 2017
+ * Description: This is an Interface that defines a Rings Property that
+ * must be implemented in any class that subscribes to it.
+ * Version: 0.1 - Created IHasRings interface
+ */
+
+namespace Assignment_4
+{
+    public interface IHasRings
+    {
+        int Rings { get; set; }
+    }
+}
