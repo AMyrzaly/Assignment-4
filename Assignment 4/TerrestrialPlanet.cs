499c73f4b249a2953eee17d2117fe12a3e7208b2
Imba-lol <best.abuka@gmail.com>
2017-07-24 17:19:08 -0400

    Created the TerrestrialPlanet class

---
+++ Assignment 4/TerrestrialPlanet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Abubakir Myrzaly
+ * Date: July 24, 2017
+ * Description: This is the TerrestrialPlanet class which is subclass of derive class. 
+ * The class will also implement both the IHasMoons and IHasRings interfaces.
+ * Version: 0.1 - Created the TerrestrialPlanet class
+ */
+
+namespace Assignment_4
+{
+    public class TerrestrialPlanet
+    {
+    }
+}
