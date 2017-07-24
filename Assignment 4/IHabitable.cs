ef667265a65d727e84c0eb67aaf2f656bd6730a0
Imba-lol <best.abuka@gmail.com>
2017-07-24 18:03:18 -0400

    Created the IHabitable interface

---
+++ Assignment 4/IHabitable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Abubakir Myrzaly
+ * Date: July 24, 2017
+ * Description: This is an Interface that defines a Habitable Property that
+ * must be implemented in any class that subscribes to it.
+ * Version: 0.1 - created the IHabitable interface
+ */
+
+namespace Assignment_4
+{
+    public interface IHabitable
+    {
+        bool Habitable { get; set; }
+    }
+}
