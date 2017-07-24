a4c805a60435174a9d6cc63dccade6b86df272a4
Imba-lol <best.abuka@gmail.com>
2017-07-23 22:41:48 -0400

    Added the private instance to the GiantPlanet class

--- Assignment 4/GiantPlanet.cs
+++ Assignment 4/GiantPlanet.cs
@@ -3,9 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+/*
+ * Name: Abubakir Myrzaly
+ * Date: July 23, 2017
+ * Description: This is the GiantPlanet class which is subclass of derive class. 
+ * The class will also implement both the IHasMoons and IHasRings interfaces.
+ * Version: 0.1 - 
+ */
+
 namespace Assignment_4
 {
-    public class GiantPlanet : Planet
+    public class GiantPlanet : Planet ,IHasMoons , IHasRings
     {
+        // PRIVATE INSTANCE VARIABLES
+
+        private string _type;
+             
+        // PUBLIC PROPERTIES
+
+        // CONSTRUCTORS
+
+        // PRIVATE METHODS
+
+        // PUBLIC METHODS
     }
 }
