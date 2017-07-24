fd894d618b22f5d67987efa787484441ebe1f5f3
Imba-lol <best.abuka@gmail.com>
2017-07-24 17:54:09 -0400

    Added the private instance and constructor to the TerrestrialPlanet class

--- Assignment 4/TerrestrialPlanet.cs
+++ Assignment 4/TerrestrialPlanet.cs
@@ -8,12 +8,34 @@ using System.Text;
  * Date: July 24, 2017
  * Description: This is the TerrestrialPlanet class which is subclass of derive class. 
  * The class will also implement both the IHasMoons and IHasRings interfaces.
- * Version: 0.1 - Created the TerrestrialPlanet class
+ * Version: 0.2 - Added the private instance and constructor
  */
 
 namespace Assignment_4
 {
-    public class TerrestrialPlanet
+    public class TerrestrialPlanet : Planet , IHasMoons, IHasRings
     {
+        // PRIVATE INSTANCE VARIABLES
+
+        private bool _oxygen;
+
+        // PUBLIC PROPERTIES
+
+
+
+        // CONSTRUCTORS
+
+        public TerrestrialPlanet(string name, double diameter, double mass, bool oxygen)
+            : base(name, diameter, mass)
+        {
+            this._oxygen = oxygen;
+        }
+
+        // PRIVATE METHODS
+
+
+
+        // PUBLIC METHODS
+
     }
 }
