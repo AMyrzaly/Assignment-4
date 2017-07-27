a3e5aad547d9ba19720a1e312ae59d8bebbb9105
Imba-lol <best.abuka@gmail.com>
2017-07-27 17:01:33 -0400

    Created the TerrestrialPlanet object in the drive class

--- Assignment 4/Program.cs
+++ Assignment 4/Program.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
  * StudentID: 300931945
  * Date: July 23, 2017
  * Description: This is the driver class for my project
- * Version: 0.2 - Created the GiantPlanet object
+ * Version: 0.3 - Created the TerrestrialPlanet object
  */
 
 namespace Assignment_4
@@ -19,6 +19,10 @@ namespace Assignment_4
         static void Main(string[] args)
         {
             GiantPlanet Mars = new GiantPlanet("Mars", 2500, 7500, "Gas");
+            Console.WriteLine(Mars);
+
+            TerrestrialPlanet Moon = new TerrestrialPlanet("Moon", 1250, 3000, true);
+            Console.WriteLine(Moon);
         }
 
         static void waitForAnyKey()
